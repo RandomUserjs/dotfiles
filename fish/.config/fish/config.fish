e0e90f47adf7591ba8a472359c6d539c5b35b18a
RandomUserpy <arthurbarrosramos7@gmail.com>
2026-02-16 10:09:30 -0300

    added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,47 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+# Auto-enable and activate virtual environment
+function venv --description "Create and activate a new virtual environment"
+    echo "Creating virtual environment in "(pwd)"/.venv"
+    python3 -m venv .venv --upgrade-deps
+    source .venv/bin/activate.fish
+
+    # Append .venv to the Git exclude file, but only if it's not
+    # already there.
+    if test -e .git
+        set line_to_append ".venv"
+        set target_file ".git/info/exclude"
+
+        if not grep --quiet --fixed-strings --line-regexp "$line_to_append" "$target_file" 2>/dev/null
+            echo "$line_to_append" >>"$target_file"
+        end
+    end
+end
+
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    # Aliases
+    alias clear "printf '\033[2J\033[3J\033[1;1H'" # fix: kitty doesn't clear properly
+    alias celar "printf '\033[2J\033[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[3J\033[1;1H'"
+    alias calear "printf '\033[2J\033[3J\033[1;1H'"
+    alias ls 'eza --icons'
+    alias pamcan pacman
+    alias q 'qs -c ii'
+
+end
